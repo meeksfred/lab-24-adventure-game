87af6a4a219f8249eee08da19cd3c6790d9e0145
meeksfred <maxfred0025@gmail.com>
2016-10-20 13:19:18 -0700

    basic scaffolding

---
+++ lab-max/webpack.config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/app/entry.js`,
+  devtool: 'eval',
+  output: {
+    filename: 'bundle.js',
+    path: 'build',
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/app/index.html`,
+    }),
+    new ExtractTextPlugin('bundle.css'),
+  ],
+  sassLoader: {
+    includePaths: [`${__dirname}/app/scss/lib`],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap'),
+      },
+      {
+        test: /\.(eot|woff|ttf|svg).*/,
+        loader: 'url?limit=10000&name=fonts/[hash].[ext]',
+      },
+    ],
+  },
+};
